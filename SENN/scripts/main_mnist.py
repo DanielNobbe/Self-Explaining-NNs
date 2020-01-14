41d1f4207abaa09422f338b7ffdf00f7768d5d49
JSGoedhart <joosje.goedhart@student.uva.nl>
2020-01-14 13:11:18 +0100

    Hide warnings

--- SENN/scripts/main_mnist.py
+++ SENN/scripts/main_mnist.py
@@ -15,6 +15,9 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <https://www.gnu.org/licenses/>.
 """
+# Ignore Future Warnings (Joosje)
+import warnings
+warnings.simplefilter(action='ignore', category=(FutureWarning, UserWarning))
 
 # Standard Imports
 import sys, os
